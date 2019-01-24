3c45959bf5263196345f044436bd8a6188438bbe
David Mayou <davidpmayou@gmail.com>
2019-01-24 16:32:22 -0600

    add get route check of returned keys

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -8,7 +8,7 @@ describe('check routes', () => {
     test(`Route GET returns array`, (done) => {
         testServer(app).get(route)
             .then((resp) => {
-                expect(ArrayisArray(resp)).toEqual(true);
+                expect(Array.isArray(resp.body)).toEqual(true);
                 done();
             });
         }
@@ -16,15 +16,16 @@ describe('check routes', () => {
     test(`Route GET has column keys if array not empty`, (done) => {
         testServer(app).get(route)
             .then((resp) => {
+                console.log('in GET test:',resp.body);
                 const returnedArrayIsCorrect = (
-                    resp.length === 0 || // if empty array, pass 
+                    resp.body.length === 0 || // if empty array, pass 
                     (
-                        resp[0].id &&
-                        resp[0].name &&
-                        resp[0].gender &&
-                        resp[0].age &&
-                        (resp[0].ready_to_transfer === true || resp[0].ready_to_transfer === false) &&
-                        (resp[0].notes === null || resp[0].notes.length > 0)
+                        resp.body[0].id &&
+                        resp.body[0].name &&
+                        resp.body[0].gender &&
+                        resp.body[0].age &&
+                        (resp.body[0].ready_to_transfer === true || resp.body[0].ready_to_transfer === false) &&
+                        (resp.body[0].notes === null || resp.body[0].notes.length > 0)
                     )
                 );
                 expect(returnedArrayIsCorrect).toEqual(true);
