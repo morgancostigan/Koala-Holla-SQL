13b9a3d1e1df9c1233dab2885ab0ec0c3d64802f
David Mayou <davidpmayou@gmail.com>
2019-01-24 13:41:56 -0600

    add test for GET route returns array

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,13 +5,21 @@ describe('check routes', () => {
     const successCode = 200;
     const route = '/api/koala';
     const koalaId = 1;
-    test(`Route GET`, (done) => {
+    test(`Route GET returns array`, (done) => {
+        testServer(app).get(route)
+            .then((resp) => {
+                expect(ArrayisArray(resp)).toEqual(true);
+                done();
+            });
+        }
+    );
+    test(`Route GET has keys if array not empty`, (done) => {
         testServer(app).get(route)
             .then((resp) => {
                 expect(resp.statusCode).toEqual(successCode);
                 done();
             });
-        }
+    }
     );
     test(`Route POST`, (done) => {
         testServer(app).post(route)
