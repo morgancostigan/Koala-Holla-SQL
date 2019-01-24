c3ebc2c65cfd3c6aeefb56e21c9ff51e72e11c1b
David Mayou <davidpmayou@gmail.com>
2019-01-24 14:09:12 -0600

    add GET return is legal object

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -13,10 +13,21 @@ describe('check routes', () => {
             });
         }
     );
-    test(`Route GET has keys if array not empty`, (done) => {
+    test(`Route GET has column keys if array not empty`, (done) => {
         testServer(app).get(route)
             .then((resp) => {
-                expect(resp.statusCode).toEqual(successCode);
+                const returnedArrayIsCorrect = (
+                    resp.length === 0 || // if empty array, pass 
+                    (
+                        resp[0].id &&
+                        resp[0].name &&
+                        resp[0].gender &&
+                        resp[0].age &&
+                        (resp[0].ready_to_transfer === true || resp[0].ready_to_transfer === false) &&
+                        (resp[0].notes === null || resp[0].notes.length > 0)
+                    )
+                );
+                expect(returnedArrayIsCorrect).toEqual(true);
                 done();
             });
         }
