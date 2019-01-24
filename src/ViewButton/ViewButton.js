d850fab2f175be9e6657be507df406614f3789ed
David Mayou <davidpmayou@gmail.com>
2019-01-24 16:46:36 -0600

    add viewButton component

--- src/ViewButton/ViewButton.js
+++ src/ViewButton/ViewButton.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import Button from '@material-ui/core/Button';
 
 class ViewButton extends Component {
+    handleClick = () => {
+        if (this.props.inputView) {
+            this.props.dispatch({ type: 'SET_LIST' });
+        } else {
+            this.props.dispatch({ type: 'SET_INPUT' });
+        }
+    }
     render() {
+        const { inputView } = this.props;
+        const buttonText = inputView ? 'Show List' : 'Add Koala';
         return (
-            <div></div>
-        )
+            <div>
+                <Button
+                    onClick={this.handleClick}
+                >{buttonText}
+                </Button>
+            </div>
+        );
     }
 } 
 
+const mapStateToProps = ({ inputView }) => ({ inputView });
 
-export default ViewButton;
+export default connect(mapStateToProps)(ViewButton);
